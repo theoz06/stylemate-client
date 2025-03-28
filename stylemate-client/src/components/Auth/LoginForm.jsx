69b0f8f6c76fa66cad297e621d77b3da979e07ab
Teofilus <t.ziraluo@yahoo.com>
2025-03-28 17:47:56 +0700

    Refactor login and registration forms; enhance styles and add background image

--- stylemate-client/src/components/Auth/LoginForm.jsx
+++ stylemate-client/src/components/Auth/LoginForm.jsx
@@ -9,22 +9,24 @@ const LoginForm = ({ formData, setFormData }) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full py-3 space-y-6">
+    <div className="flex flex-col items-center justify-center w-full pt-3 space-y-6 ">
       <input
+        autoComplete="off"
         type="text"
         name="username"
         value={formData.username || ""}
         onChange={handleInputChange}
         placeholder="Username"
-        className="bg-gray-200 p-3 rounded-full h-[3rem] w-60 focus:outline-offset-2 focus:outline-2 focus:outline-gray-400"
+        className="bg-gray-200 p-3 rounded-full h-[3rem]  focus:outline-offset-2 focus:outline-2 focus:outline-gray-400"
       />
       <input
+        autoComplete="off"
         type="password"
         name="password"
         value={formData.password  || ""}
         onChange={handleInputChange}
         placeholder="Password"
-        className="bg-gray-200 p-3 rounded-full h-[3rem] w-60 focus:outline-offset-2 focus:outline-2 focus:outline-gray-400"
+        className="bg-gray-200 p-3 rounded-full h-[3rem] focus:outline-offset-2 focus:outline-2 focus:outline-gray-400"
       />
     </div>
   );
