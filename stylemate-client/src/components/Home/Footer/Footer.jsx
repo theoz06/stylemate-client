4e3896587d8c23bbd3014f568c49949ac5a3acda
Teofilus <t.ziraluo@yahoo.com>
2025-03-30 09:54:44 +0700

    Add RiAiGenerate icon to Footer component

--- stylemate-client/src/components/Home/Footer/Footer.jsx
+++ stylemate-client/src/components/Home/Footer/Footer.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import footer from "@/components/Home/Footer/footer.module.css";
 import { IoMdHome, IoShirt } from "react-icons/io";
+import { RiAiGenerate } from "react-icons/ri";
 import { PiShuffleBold } from "react-icons/pi";
 
 
