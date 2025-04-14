6a49785165a127443b9804fcdd4e7985aff99758
Teofilus <t.ziraluo@yahoo.com>
2025-04-14 18:00:09 +0700

    Implement Modal component and enhance Wardrobe functionality with content display and animations; adjust sidebar transition delay for improved user experience

--- stylemate-client/src/components/Main/Wardrobe/index.jsx
+++ stylemate-client/src/components/Main/Wardrobe/index.jsx
@@ -1,10 +1,34 @@
 import Header from "@/components/header";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import index from "./index.module.css";
 import { categories } from "@/libs/datas/sideBarMenu";
+import Modal from "@/components/modal";
 
 const Wardrobe = () => {
   const [activeSideMenu, setActiveSideMenu] = useState(1);
+  const [showContent, setShowContent] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  const onOpen = () => {
+    setIsOpen(true);
+  };
+  
+
+  useEffect(() => {
+    setShowContent(false);
+
+    const timer = setTimeout(() => {
+      setShowContent(true);
+    }, 1200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [activeSideMenu]);
 
   return (
     <article className="relative w-full min-h-screen flex flex-col items-center ">
@@ -12,33 +36,71 @@ const Wardrobe = () => {
 
       <main className="w-full h-screen flex items-center justify-center text-[#123458]">
         <div className="relative w-full h-full flex pt-7">
-          <aside className={`${index.sidebar_content} sidebar sidebar-container w-[70px] flex-wrap bg-[#123458] text-[#05182d] flex flex-col items-center shadow-[5px_0_5px_-2px_rgba(0,0,0,0.3)]`}>
+          <aside
+            className={`${index.sidebar_content} sidebar sidebar-container w-[70px] flex-wrap bg-[#123458] text-[#05182d] flex flex-col items-center shadow-[5px_0_5px_-2px_rgba(0,0,0,0.3)]`}
+          >
             <div className="w-full flex flex-col items-center">
               <ul
                 className={`${index.list} sidebar-list h-full w-full p-6 flex flex-col items-center gap-3 text-center text-sm font-semibold`}
               >
-                {[...categories].sort((a, b) => a.id - b.id).map((category) => (
-                  <li key={category.id}>
-                  <button type="button" className={`flex flex-col items-center justify-center transition-all ease-in-out delay-100 duration-400 ${activeSideMenu === category.id ? index.active : ""}`} onClick={() => setActiveSideMenu(category.id)}>
-                    {category.img}
-                    {category.title}
-                  </button>
-                </li>
-                ))}
+                {[...categories]
+                  .sort((a, b) => a.id - b.id)
+                  .map((category) => (
+                    <li key={category.id}>
+                      <button
+                        type="button"
+                        className={`flex flex-col items-center justify-center transition-all ease-in-out duration-200 ${
+                          activeSideMenu === category.id ? index.active : ""
+                        }`}
+                        onClick={() => {
+                          setShowContent(false);
+                          setTimeout(() => {
+                            setActiveSideMenu(category.id);
+                          }, 300);
+                        }}
+                      >
+                        {category.img}
+                        {category.title}
+                      </button>
+                    </li>
+                  ))}
               </ul>
             </div>
           </aside>
-          <div className="w-full h-full flex flex-col items-center justify-center">
-            <h1 className="text-2xl font-bold">{activeSideMenu === 1 && "Top"} {activeSideMenu === 2 && "Bottom"} {activeSideMenu === 3 && "Dress"} {activeSideMenu === 4 && "Bags"} {activeSideMenu === 5 && "Shoes"} {activeSideMenu === 6 && "Others"} </h1>
-            <p className="text-sm ">Ini adalah halaman wardrobe.</p>
+
+          <div className="content-container w-full h-full flex flex-col items-center justify-center">
+              <div className={`content w-full h-full flex flex-col items-center justify-center transition duration-200 ease-in-out
+    ${showContent ? "opacity-100 translate-y-0" : "opacity-0 translate-y-2"}
+  `}>
+                <h1 className="text-2xl font-bold">
+                  {activeSideMenu === 1 && "Top"}{" "}
+                  {activeSideMenu === 2 && "Bottom"}{" "}
+                  {activeSideMenu === 3 && "Dress"}{" "}
+                  {activeSideMenu === 4 && "Bags"}{" "}
+                  {activeSideMenu === 5 && "Shoes"}{" "}
+                  {activeSideMenu === 6 && "Others"}{" "}
+                </h1>
+                <p className="text-sm ">Ini adalah halaman wardrobe.</p>
+              </div>
+            
           </div>
         </div>
       </main>
       <button
+        type="button"
+        onClick={onOpen}
+        aria-label="Add New Item"
         className={`${index.button} shadow-md shadow-gray-700 rounded-full absolute bottom-20 right-3 w-15 h-15 text-center p-2 text-5xl font-bold text-[#F1EFEC] bg-[#123458]`}
       >
         +
       </button>
+      {isOpen && (
+        <Modal title={"Add New Item"} onClose={onClose} isOpen={isOpen}>
+        <div>
+          Modal
+        </div>
+      </Modal>
+      )}
     </article>
   );
 };
